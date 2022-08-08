5e4e70282fb0f710df240fba553980b4c3d79b55
elzup <guild0105@gmail.com>
2022-08-08 14:31:43 +0900

    add mjs export

--- src/emojify.ts
+++ src/emojify.ts
@@ -1,23 +1,23 @@
 import { readFileSync } from 'fs'
 
 import * as url from 'url'
-import toHiragana from 'jaco/fn/toHiragana'
-
+import { toHiragana } from 'jaco'
 import { tokenize } from 'kuromojin'
 
 const dirname = url.fileURLToPath(new URL('.', import.meta.url))
+const toHira = (text: string) => toHiragana(text)
 
-const emojiTsv = readFileSync(dirname + './resource/emoji.tsv', 'utf8')
-const toHira = (text: string) => {
-  return toHiragana(text)
+const loadLib = () => {
+  const emojiTsv = readFileSync(dirname + './resource/emoji.tsv', 'utf8')
+
+  return Object.fromEntries(
+    emojiTsv
+      .trim()
+      .split('\n')
+      .map((v) => v.split('\t'))
+  )
 }
-
-const lib = Object.fromEntries(
-  emojiTsv
-    .trim()
-    .split('\n')
-    .map((v) => v.split('\t'))
-)
+const lib = loadLib()
 
 export const emojify = async (text: string) => {
   const tokens = await tokenize(text)
